f0224c6fe65812122173a49ed7536b11c0b3c228
lllllyaSh <100190200+lllllyaSh@users.noreply.github.com>
2023-07-11 11:25:52 +0300

    Add files via upload

---
+++ blog/api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework import generics
+from api.models import Post
+from api import serializers
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class =serializers.PostSerializer
